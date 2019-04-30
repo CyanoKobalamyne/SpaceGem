bf8b2dc3b4dd1758808cf2c6a194004bfa5f7d1c
Adeeb725 <31573410+Adeeb725@users.noreply.github.com>
2019-04-30 14:04:52 -0400

    Add Level 1
    Scene 1 - Adeeb Level 1

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+
+    private LevelManager theLevelManager;
+    public int gemValue;
+
+    // Use this for initialization
+    void Start()
+    {
+        theLevelManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            theLevelManager.AddGems(gemValue);
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public float waitToRespawn;
+    public PlayerController thePlayer;
+
+    public GameObject deathSplosion;
+
+    public int gemCount;
+
+    public Text gemText;
+
+    public Image life1;
+    public Image life2;
+    public Image life3;
+
+    public Sprite lifeFull;
+    public Sprite lifeHalf;
+    public Sprite lifeEmpty;
+
+    public int maxHealth;
+    public int healthCount;
+
+    private bool respawning;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+
+        gemText.text = "Gems: " + gemCount;
+
+        healthCount = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthCount <= 0 && !respawning)
+        {
+            Respawn();
+            respawning = true;
+        }
+    }
+
+    public void Respawn()
+    {
+        StartCoroutine("RespawnCo");
+    }
+
+    public IEnumerator RespawnCo()
+    {
+        thePlayer.gameObject.SetActive(false);
+
+        Instantiate(deathSplosion, thePlayer.transform.position, thePlayer.transform.rotation);
+
+        yield return new WaitForSeconds(waitToRespawn);
+
+        healthCount = maxHealth;
+        respawning = false;
+
+        thePlayer.transform.position = thePlayer.respawnPosition;
+        thePlayer.gameObject.SetActive(true);
+    }
+
+    public void AddGems(int gemsToAdd)
+    {
+        gemCount += gemsToAdd;
+
+        gemText.text = "Gems: " + gemCount;
+
+    }
+
+    public void HurtPlayer(int damageToTake)
+    {
+        healthCount -= damageToTake;
+    }
+}
